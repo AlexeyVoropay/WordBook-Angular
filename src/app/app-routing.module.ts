4dadc7023dc5b542032552b1e289f5dbcbb02759
alexey <monkena@yandex.ru>
2019-07-10 19:34:14 +0300

    Add a navigation link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LanguagesComponent }      from './languages/languages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'languages', component: LanguagesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
