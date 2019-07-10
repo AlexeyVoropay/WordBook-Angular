691734681e357ababa6c221240d1d00ff518e553
Alexey Voropay <monkena@yandex.ru>
2019-07-10 10:40:58 +0300

    Show messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LanguagesComponent } from './languages/languages.component';
 import { LanguageDetailComponent } from './language-detail/language-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LanguagesComponent,
-    LanguageDetailComponent
+    LanguageDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/language.service.ts
+++ src/app/language.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Language } from './language';
 import { LANGUAGES } from './mock-languages';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,10 @@ import { Observable, of } from 'rxjs';
 export class LanguageService {
 
   getLanguages(): Observable<Language[]> {
+	// TODO: send the message _after_ fetching the languages
+    this.messageService.add('LanguageService: fetched languages');
 	return of(LANGUAGES);
-  }
+  }  
   
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
