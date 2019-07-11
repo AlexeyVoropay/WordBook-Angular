d1c66d7ac6d37f5e93b3736733770848e58aa9c1
alexey <monkena@yandex.ru>
2019-07-11 23:58:22 +0300

    Get heroes with HttpClient(fix errors via code from future)

--- src/app/language.service.ts
+++ src/app/language.service.ts
@@ -1,28 +1,60 @@
 import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Language } from './language';
-import { LANGUAGES } from './mock-languages';
 import { MessageService } from './message.service';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService {
+	
+  private languagesUrl = 'api/languages';  // URL to web api
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
 
+  /** GET languages from the server */
   getLanguages(): Observable<Language[]> {
-    // TODO: send the message _after_ fetching the languages
-    this.messageService.add('LanguageService: fetched languages');
-	  return of(LANGUAGES);
+	  return this.http.get<Language[]>(this.languagesUrl)
   }  
   
+  /** GET hero by id. Will 404 if id not found */
   getLanguage(id: number): Observable<Language> {
-    // TODO: send the message _after_ fetching the language
-    this.messageService.add(`LanguageService: fetched language id=${id}`);
-    return of(LANGUAGES.find(language => language.id === id));
-  }  
+    const url = `${this.languagesUrl}/${id}`;
+    return this.http.get<Language>(url).pipe(
+      tap(_ => this.log(`fetched language id=${id}`))
+	  ,catchError(this.handleError<Language>(`getLanguage id=${id}`)
+	  )
+    );
+  }
+  
+   /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+ 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+ 
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+ 
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`LanguageService: ${message}`);
+  }
   
 }
