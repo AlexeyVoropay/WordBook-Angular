aa80b62cf24b73ce917021ada459bd96ee32ad8e
alexey <monkena@yandex.ru>
2020-03-05 21:27:00 +0300

    change search language route

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,4 @@
 //export const apiHost = 'http://localhost:50473/api/';  // URL to web api
 //export const apiHost = 'http://alexey.somee.com/api/';  // URL to web api
-export const apiHost = 'http://monkena.azurewebsites.net/api/';  // URL to web api
+//export const apiHost = 'http://monkena.azurewebsites.net/api/';  // URL to web api
+export const apiHost = 'http://monkena-api-core.azurewebsites.net/api/';  // URL to web api
